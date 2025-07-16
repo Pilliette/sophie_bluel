ec63e5db279c1abce1bad1eecfb88bd59418e0cc
Pilliette <acrmbz@gmail.com>
2025-07-17 00:52:07 +0200

    WIP affichage de la modale

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import des fichiers galerie.js / filtres.js complets
 import galerie from './JS/galerie.js'
 import filtres from './JS/filtres.js'
+import modale from './JS/modale.js'
 
 // Appel de la fonction pour charger la galerie
 galerie.chargerGalerie(galerie.allWorks)
