22ae7d3e8ac45ae8d798902175db9202666471d8
Pilliette <acrmbz@gmail.com>
2025-08-26 18:14:27 +0200

    Commentaires du fichier deconnexion.js

--- JS/deconnexion.js
+++ JS/deconnexion.js
@@ -1,13 +1,17 @@
+// Redirection vers la page d'accueil si aucun token n'est enregistré dans le localStorage
 if (document.body.classList.contains(`relativeBody`) && !localStorage.getItem(`token`)) {
     window.location.href = `../index.html`
 }
 
+// Sélection de .logout
 const logout = document.querySelector(`.logout`)
 
 if (logout) {
     
+    // Écoute du clic de .logout
     logout.addEventListener(`click`, (event) => {
 
+        // Empêchement du rechargement de la page
         event.preventDefault()
 
         // Suppression du token d'authentification dans le localStorage
